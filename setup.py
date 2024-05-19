46227a31c485cfbb8d26367a8d5b20fad18e0e74
Oussama Jarrousse <oussama@jarrousse.org>
2024-05-19 12:47:18 +0200

    fixed package version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=open("README.md").read(),
     keywords="pytest minio mock",
     extras_require={"dev": ["pre-commit", "tox"]},
-    version="0.4.13",
+    version="0.4.14",
     long_description_content_type="text/markdown",
     classifiers=[
         "Framework :: Pytest",
